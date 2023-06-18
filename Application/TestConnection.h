6bada1b1b39e3f94ce7e6d57996f32f4f3467fd4
rernamp <renamp100@gmail.com>
2023-06-18 12:25:59 +0300

    add send array to TestConnection

--- Application/TestConnection.h
+++ Application/TestConnection.h
@@ -5,19 +5,23 @@
 
 #include <Eni/Threading/Thread.h>
 
+#include <array>
+
 namespace UDA {
     class TestConnection : public IConnection {
     public:
         TestConnection() {            
+        	for (std::size_t i = 0; i < _data.size(); i++) {
+        		_data[i] = i;
+        	}
         }
 
         void process(ITransmitter* transmitter) override {
             _transmitter = transmitter;
-
+            exitRequest = false;
             while(!exitRequest) {
                 using namespace Eni;
-                ++data;
-                _transmitter->append(&data, 1);
+                _transmitter->append(_data.data(), _data.size());
                 Threading::ThisThread::sleepForMs(10);
                 
             }
@@ -28,7 +32,8 @@ namespace UDA {
         }
     private:
         bool exitRequest = false;
-        uint8_t data = 0;
+        static constexpr std::size_t sizeTestData = 256;
+        std::array<uint8_t, sizeTestData> _data = {};
         ITransmitter* _transmitter = nullptr;
     };
 }
